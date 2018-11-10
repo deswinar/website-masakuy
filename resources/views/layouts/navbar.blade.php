623311c51752b97af2714986e98b175a174f233a
deswinar <deswinauliyaarabbani@gmail.com>
2018-11-10 08:14:52 +0700

    First commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/">
+    <img src="http://goodshepherdchurch.net/wp-content/uploads/2014/06/icon_50431.png" width="30" height="30" alt="">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item {{Request::is('/') ? 'active' : ''}}">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item {{Request::is('explore') ? 'active' : ''}}">
+        <a class="nav-link" href="/explore">Explore</a>
+      </li>
+      <li class="nav-item {{Request::is('about') ? 'active' : ''}}">
+        <a class="nav-link" href="/about">About</a>
+      </li>
+      <li class="nav-item {{Request::is('create') ? 'active' : ''}}">
+        <a class="nav-link" href="/create">Create</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
